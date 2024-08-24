5c448acd49b7a8010b17d1ecab7123af1cf4ea44
ajmalj05 <ajmalj2003@gmail.com>
2024-08-24 16:22:01 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,104 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+  sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }                                                    
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+
+//form validation
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('myForm');
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    
+
+    const usernameError = document.getElementById('usernameError');
+    const emailError = document.getElementById('emailError');
+   
+
+    form.addEventListener('submit', (event) => {
+        let isValid = true;
+
+        // Clear previous error messages
+        usernameError.textContent = '';
+        emailError.textContent = '';
+        
+
+        // Validate username
+        if (username.value.trim() === '') {
+            usernameError.textContent = 'Username is required.';
+            isValid = false;
+        }
+
+        // Validate email
+        if (!validateEmail(email.value)) {
+            emailError.textContent = 'Please enter a valid email address.';
+            isValid = false;
+        }
+
+       
+        
+
+        if (!isValid) {
+            event.preventDefault(); // Prevent form submission if validation fails
+        }
+    });
+
+    function validateEmail(email) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(String(email).toLowerCase());
+    }
+});
